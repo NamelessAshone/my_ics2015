a4c738d1f20f8fdef454bbdd68b7c421eb63ccd1
tracer-ics2015 <tracer@njuics.org>
2017-09-10 00:18:10 +0800

    >  compile NEMU 42 ash Linux debian 4.9.0-3-686 #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  00:18:10 up  3:39,  3 users,  load average: 0.02, 0.05, 0.01 1646a5c627172bf7344b79146a8a1aa4adea195

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -23,8 +23,9 @@ typedef struct {
 		} gpr[8];
 
 		/* Do NOT change the order of the GPRs' definitions. */
-		
-		uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	    struct{
+			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		};
 	};
 
 	swaddr_t eip;
