a37ba0c063e9a53d26397d7a6780770e2be990c2
tracer-ics2015 <tracer@njuics.org>
2017-09-10 22:44:45 +0800

    >  compile NEMU 42 ash Linux debian 4.9.0-3-686 #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  22:44:45 up 14:48,  3 users,  load average: 0.18, 0.10, 0.03 d700cee66a9ed6c5f5402e862786306588dfe4f7

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -40,20 +40,22 @@ static int cmd_si(char *args) {
 	return 0;
 }
 static int cmd_info(char *args) {
-	if(strcmp(args,"r") == 0) {
-		printf("EAX %X\tECX %X\tEDX %X\tEBX %X\n",
+	if(args == NULL) {
+	
+	}else if(strcmp(args,"r") == 0) {
+		printf("EAX: %X\tECX: %X\tEDX: %X\tEBX: %X\n",
 			   cpu.eax,cpu.ecx,cpu.edx,cpu.ebx);
-		printf("ESP %X\tEBP %X\tESI %X\tEDI %X\n",
+		printf("ESP: %X\tEBP: %X\tESI: %X\tEDI: %X\n",
 			   cpu.esp,cpu.ebp,cpu.esi,cpu.edi);
-		printf("AX  %X\tCX  %X\tDX  %X\tBX  %X\n",
+		printf("AX:  %X\tCX:  %X\tDX:  %X\tBX:  %X\n",
 			   reg_w(R_AX),reg_w(R_CX),reg_w(R_DX),reg_w(R_BX));
-		printf("SP  %X\tDP  %X\tSI  %X\tDI  %X\n",
+		printf("SP:  %X\tDP:  %X\tSI:  %X\tDI:  %X\n",
 		       reg_w(R_SP),reg_w(R_BP),reg_w(R_SI),reg_w(R_DI));
-		printf("AH  %X\tCH  %X\tDH  %X\tBH  %X\n",
+		printf("AH:  %X\tCH:  %X\tDH:  %X\tBH:  %X\n",
 			   reg_b(R_AH),reg_b(R_CH),reg_b(R_DH),reg_b(R_BH));
-		printf("AL  %X\tCL  %X\tDL  %X\tBL  %X\n",
+		printf("AL:  %X\tCL:  %X\tDL:  %X\tBL:  %X\n",
 			   reg_b(R_AL),reg_b(R_CL),reg_b(R_DL),reg_b(R_BL));
-		printf("ok\n");
+
 	}
 	return 0;
 }
