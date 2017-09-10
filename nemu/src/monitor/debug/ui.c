12e9d9cedb1bf7aa66995b253db3a59707147573
tracer-ics2015 <tracer@njuics.org>
2017-09-10 21:06:29 +0800

    >  compile NEMU 42 ash Linux debian 4.9.0-3-686 #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  21:06:29 up 13:09,  3 users,  load average: 0.00, 0.00, 0.00 ed6b20aeba0cc927966ca3221af43fda8c88969

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -35,7 +35,10 @@ static int cmd_c(char *args) {
 static int cmd_q(char *args) {
 	return -1;
 }
-
+static int cmd_si(char *args) {
+	cpu_exec(1);
+	return 0;
+}
 static int cmd_help(char *args);
 
 static struct {
@@ -46,7 +49,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-
+	{ "si", "Run by sigle-step", cmd_si},
 	/* TODO: Add more commands */
 
 };
