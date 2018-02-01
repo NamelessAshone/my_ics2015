b5e7594f31bed46ed7c6b4cbeeb890a0988ed12c
tracer-ics2015 <tracer@njuics.org>
2018-02-01 20:37:04 +0800

    >  compile NEMU 42 ash Linux debian 4.9.0-3-686 #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  20:37:04 up 1 day, 11:29, 10 users,  load average: 0.00, 0.00, 0.00 2ed22c7a50f7bc0f1f4d6d0a5ba1d66a4293d8d

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -192,9 +192,9 @@ uint32_t expr(char *e, bool *success) {
 	/* TEST: */
 	printf("[[%d]]\n",nr_token);
 	if(check_parentheses(0, nr_token - 1))
-		printf("\033[30;43m[matched\n]");
+		printf("\33[30;43mmatched\n");
 	else
-		printf("\033[30:43m[no matched\n]"); 
+		printf("\33[30;43mno matched\n"); 
 
 	/* TODO: Insert codes to evaluate the expression. */
 	
