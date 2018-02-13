9dad2e0bf6362ad70a4c400f361df6e1842abbc8
tracer-ics2015 <tracer@njuics.org>
2018-02-13 22:43:05 +0800

    >  compile NEMU 42 ash Linux debian 4.9.0-3-686 #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  22:43:05 up 2 days,  3:37, 10 users,  load average: 0.00, 0.00, 0.00 df7a3b9bf12510b35aca97d44f45b80a841a72d7

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -267,7 +267,7 @@ uint32_t expr(char *e, bool *success) {
 		if(i % 10 == 9) {
 			printf("\n");
 			int j = i - 9;
-			while((j - 1) % 10 != 9) 
+			while(j != i + 1) 
 				printf("%d\t", j++);
 			printf("\n");
 		}
