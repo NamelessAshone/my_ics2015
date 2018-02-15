e0f7121f3e2fd2d9ad15eeef05aaeeaf094fa378
tracer-ics2015 <tracer@njuics.org>
2018-02-15 16:02:32 +0800

    >  compile NEMU 42 ash Linux debian 4.9.0-3-686 #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  16:02:32 up 2 days, 10:53, 10 users,  load average: 0.00, 0.02, 0.00 34beecf5ae6cba786a9e64f311022eba6061b55

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -385,10 +385,10 @@ uint32_t expr(char *e, bool *success) {
 	/* TODO: Insert codes to evaluate the expression. */
 	int i = 0;
 	for(i = 0; i < nr_token; i++) {
-		if(tokens[i].type == '*' && (i == 0 || tokens[i - 1].type == NUM || tokens[i - 1].type == HEX || tokens[i - 1].type == REG)) {
+		if(tokens[i].type == '*' && (i == 0 || tokens[i - 1].type != NUM || tokens[i - 1].type != HEX || tokens[i - 1].type != REG )) {
 			tokens[i].type = DEREF;
 		}
-		if(tokens[i].type == '-' && (i == 0 || tokens[i - 1].type == NUM || tokens[i - 1].type == HEX || tokens[i - 1].type == REG)) {
+		if(tokens[i].type == '-' && (i == 0 || tokens[i - 1].type != NUM || tokens[i - 1].type != HEX || tokens[i - 1].type != REG)) {
 			tokens[i].type = MINUS;
 		}
 	}
